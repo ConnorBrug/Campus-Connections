7b33424b637671c503f038fe11d0ca3b998a9e41
Michael Razavi <michaelrazavi2@gmail.com>
2025-08-08 17:43:29 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration is now sourced from environment variables
+// Your web app's Firebase configuration is now hardcoded to ensure it is always available.
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "connections-hw9ha",
+  "appId": "1:689188733774:web:5dfbbfa493a7d8f2c4141f",
+  "storageBucket": "connections-hw9ha.firebasestorage.app",
+  "apiKey": "AIzaSyBKUwJiswvgULDhk-jb4kGharvZdl29_EM",
+  "authDomain": "connections-hw9ha.firebaseapp.com",
+  "messagingSenderId": "689188733774"
 };
 
 // Initialize Firebase for client-side
