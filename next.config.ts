1130049a7932a765805dee70ec291c8db648fc91
Michael Razavi <michaelrazavi2@gmail.com>
2025-10-12 04:20:52 +0000

    Try fixing this error: `Console Error: Error: 2 UNKNOWN: Getting metadat

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,6 @@ const nextConfig: NextConfig = {
       "https://9000-firebase-studio-1749349277265.cluster-hf4yr35cmnbd4vhbxvfvc6cp5q.cloudworkstations.dev",
       "https://3000-firebase-studio-1752880504974.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev",
       "https://9000-firebase-studio-1752880504974.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev",
-      "https://3001-firebase-studio-1752880504974.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev", // 👈 new
     ],
   },
   
